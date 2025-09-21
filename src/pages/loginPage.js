0d390589c2cf23427afd5ea063bce85a02eb2e71
isnardnetto <engisnard@gmail.com>
2025-09-21 17:18:21 -0300

    Adicionando before e afterEach

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,4 +1,6 @@
-class LoginPage {
+import { expect } from "@playwright/test";
+
+export class LoginPage {
   constructor(page) {
     this.page = page;
   }
@@ -6,6 +8,14 @@ class LoginPage {
   async login(userName, password) {
     await this.page.getByTestId("email").fill(userName);
     await this.page.getByTestId("senha").fill(password);
-    await this.getByTestId("entrar").click();
+  }
+
+  async validateLoginError(texto) {
+    const validateErrorTest = this.page.getByText("Email e/ou senha inválidos");
+    await expect(validateErrorTest).toContainText(texto);
+  }
+
+  async clicarBtnEntrar() {
+    await this.page.getByTestId("entrar").click();
   }
 }
---
+++ src/tests/loginPage.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/loginPage.js";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://front.serverest.dev/login");
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+  await page.screenshot({ path: `screenshots/${testInfo.title}.png` });
+  await page.close();
+});
+
+test("loginPage - sucess", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login("teste@gmail.com", "12345");
+  await loginPage.clicarBtnEntrar();
+});
+
+test("loginPage - error ", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login("testeErro@gmail.com", "112233");
+  await loginPage.clicarBtnEntrar();
+  await loginPage.validateLoginError("Email e/ou senha inválidos");
+});
+
+test("Login em branco", async ({ page }) => {});
+
+test("Senha em branco", async ({ page }) => {});
+
+test("Cadastro de novo usuário", async ({ page }) => {});
